4c761443651d9198b1e37c1f5b0583df34f16ac0
flppetkovski <flp_petkovski@yahoo.com>
2021-03-23 20:06:01 +0100

    talk-tank deployment

---
+++ util/check-auth.js
@@ -0,0 +1,25 @@
+const AuthenticationError = require("apollo-server")
+const jwt = require("jsonwebtoken")
+const {SECRET} = require("../config")
+
+module.exports = (context) => {
+const authHeader = context.req.headers.authorization
+
+if (authHeader) {
+  const token = authHeader.split("Bearer ")[1]
+
+  if (token) {
+   try {
+    const user = jwt.verify(token, SECRET)
+return user
+
+   } catch (error) {
+    throw new AuthenticationError("Invalid / Expired Token")
+   }
+   
+  }
+  throw new Error("Authorization header must be provided")
+}
+throw new Error ("Authentication token must be \'Bearer [token] \'")
+
+}
